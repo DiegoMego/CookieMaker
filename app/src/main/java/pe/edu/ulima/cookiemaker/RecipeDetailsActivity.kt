cfaaeca66a3bf42aa3a27f8ee087c95609f24ec5
Diego Mego <DM2593@gmail.com>
2021-10-03 03:12:59 +0000

    feat: recipe details layout done. Also added the related activity with no functionality.

---
+++ app/src/main/java/pe/edu/ulima/cookiemaker/RecipeDetailsActivity.kt
@@ -0,0 +1,11 @@
+package pe.edu.ulima.cookiemaker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecipeDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_details)
+    }
+}
