73ac729d9fdd0a81e5184a032a07727c1aeb018a
DiegoMego <30615834+DiegoMego@users.noreply.github.com>
2021-10-08 21:40:21 +0000

    feat: implemented recycleview for recipes items and its layout. Added the adapter and the listener interface.

---
+++ app/src/main/java/pe/edu/ulima/cookiemaker/adapter/RecipeListAdapter.kt
@@ -0,0 +1,54 @@
+package pe.edu.ulima.cookiemaker.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import pe.edu.ulima.cookiemaker.R
+import pe.edu.ulima.pm.ulgamestore.model.Receta
+
+class RecipeListAdapter(
+    private val recipeList :List<Receta>,
+    private val fragment : Fragment,
+    private val listener : (Receta) -> Unit) :
+        RecyclerView.Adapter<RecipeListAdapter.ViewHolder>() {
+            class ViewHolder(
+                view : View, val listener : (Receta) -> Unit, val recipeList: List<Receta>
+            ) : RecyclerView.ViewHolder(view), View.OnClickListener {
+
+                val iviRecipeImage : ImageView
+                val tviRecipeName : TextView
+                val tviUserName : TextView
+
+                init {
+                    iviRecipeImage = view.findViewById(R.id.iviRecipeImage)
+                    tviRecipeName = view.findViewById(R.id.tviRecipeName)
+                    tviUserName = view.findViewById(R.id.tviUserName)
+                    view.setOnClickListener(this)
+                }
+
+                override fun onClick(v: View?) {
+                    listener(recipeList[adapterPosition])
+                }
+            }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_recipe_item, parent, false)
+
+        return ViewHolder(view, listener, recipeList)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tviRecipeName.text = recipeList[position].nombre
+        holder.tviUserName.text = recipeList[position].usuario
+        holder.iviRecipeImage.setImageResource(R.drawable.galletas_1)
+    }
+
+    override fun getItemCount(): Int {
+        return recipeList.size
+    }
+}
